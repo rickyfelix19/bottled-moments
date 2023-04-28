d9d6bd893caad32feb9d5cd9c4bef3917b6d7ef6
Ricky Felix <rfel4230@uni.sydney.edu.au>
2023-04-28 11:49:20 +1000

    add unit testing files

---
+++ client-sender/src/testing.js
@@ -0,0 +1,7 @@
+// the file here is used to conduct unit testing to Resolume
+
+/*
+    * Use case 1: send artifact
+    * Use case 2: send multiple artifacts within a few minutes duration
+    * Use case 3: Special effects 
+*/
