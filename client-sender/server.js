41afab5bfef7750c553152c8ce741c7b0477f2d8
Ricky Felix <rfel4230@uni.sydney.edu.au>
2023-05-02 23:12:52 +1000

    initialize commit

--- client-sender/server.js
+++ client-sender/server.js
@@ -1,18 +1,17 @@
 // Port for the Express web server
-var PORT = 4040;
+var PORT = process.env.PORT || 4040;
 
 // Import Express and initialise the web server
-var express = require('express');
+var express = require("express");
 var app = express();
 var server = app.listen(PORT);
-app.use(express.static('public'));
-console.log('Node.js Express server running on port ' + PORT);
+app.use(express.static("public"));
+console.log("Node.js Express server running on port " + PORT);
 
 // Import and configure body-parser for Express
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended:false}));
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 
 /*
     Create an osc.js UDP Port listening on port 57121.
@@ -21,24 +20,23 @@ app.use(bodyParser.json());
 */
 var osc = require("osc");
 var udpPort = new osc.UDPPort({
-    localAddress: "0.0.0.0",
-    localPort: 57121,
-    metadata: true
+  localAddress: "0.0.0.0",
+  localPort: 57121,
+  metadata: true,
 });
 
 // Open the socket.
 udpPort.open();
 
 // Handle POST requests
-app.post('/sendMessage', function(request, response) {
-	var address = request.body.address;
-	var value = request.body.value;
-    var type = request.body.type;
+app.post("/sendMessage", function (request, response) {
+  var address = request.body.address;
+  var value = request.body.value;
+  var type = request.body.type;
 
-	sendOSC(address, value, type);
-    response.end("");
+  sendOSC(address, value, type);
+  response.end("");
 });
-
 
 /*
     Send OSC messages
@@ -46,16 +44,19 @@ app.post('/sendMessage', function(request, response) {
    * 127.0.0.1 is localhost
 */
 function sendOSC(address, value, type) {
-    // console.log('OSC message sent: address: ' + address + ', value: ' + value);
-    udpPort.send({
-        address: address,
-        args: [
-            {
-//                type: "f",
-                type: type,
-                value: value
-            }
-        ]
-    }, "127.0.0.1", 7000);
+  // console.log('OSC message sent: address: ' + address + ', value: ' + value);
+  udpPort.send(
+    {
+      address: address,
+      args: [
+        {
+          //                type: "f",
+          type: type,
+          value: value,
+        },
+      ],
+    },
+    "127.0.0.1",
+    7000
+  );
 }
-
