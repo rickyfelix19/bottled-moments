a656866cf763869da217a3f975fa51e208c2691b
Ricky Felix <rfel4230@uni.sydney.edu.au>
2023-05-02 23:21:35 +1000

    fix node server issue

--- client-sender/src/main.js
+++
@@ -1,7 +0,0 @@
-import "./global.css";
-
-// this code below allow Cypress to be used with Cucumber for making BDD and User Stories
-const cucumber = require("cypress-cucumber-preprocessor").default;
-module.exports = (on, config) => {
-  on("file: preprocessor", cucumber());
-};
