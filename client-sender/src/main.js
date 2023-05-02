2fedbf1f7bc120519cdc2ffbf489734b83775298
Ricky Felix <rfel4230@uni.sydney.edu.au>
2023-05-02 17:32:36 +1000

    final version

--- client-sender/src/main.js
+++ client-sender/src/main.js
@@ -1,8 +1,7 @@
-import './global.css'
-
+import "./global.css";
 
 // this code below allow Cypress to be used with Cucumber for making BDD and User Stories
-const cucumber = require(“cypress - cucumber - preprocessor”).default;
+const cucumber = require("cypress-cucumber-preprocessor").default;
 module.exports = (on, config) => {
-    on(“file: preprocessor”, cucumber());
+  on("file: preprocessor", cucumber());
 };
