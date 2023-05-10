71528c9c699785f194d7433286166425509bf437
Ricky Felix <rfel4230@uni.sydney.edu.au>
2023-05-10 20:25:26 +1000

    last night edit

--- public/pages/3_sketch/sketch.js
+++ public/pages/3_sketch/sketch.js
@@ -1,87 +1,196 @@
-
 // Fixed variables
 var HOST = window.location.origin;
 let xmlHttpRequest = new XMLHttpRequest();
 
-let mode = 0;
-let userArray = [];
+// change screen display
+var screenMode = 0;
+
+// save user selection
+let userStorage = [];
+
+// carousel
+let leftArrow, rightArrow, 
+
+function preload() {
+  let products = loadJSON('./product.json')
+}
 
 function setup() {
   p5=createCanvas(windowWidth, windowHeight);
   p5.parent('container-p5');
 
-  initialiseResolume();
+  // let nextPage = primaryButton()
+  // let prevPage = previousButton()
+  // let sendLeft = leftScreen()
+  // let sendRight = rightScreen()
+
+  // initialiseResolume();
+
+    // createCanvas(windowWidth, windowHeight);
   let myButton = primaryButton()
-  
 }
 
 function draw() {
-  // page 1 button 1 clicked -> page 2
-  // page 2 button 1 clicked -> page 1, page 2 button 2 clicked -> page 3
-  // page 3 button 1 clicked -> page 2, page 3 button 2 clicked -> send request
-    if (mode == 0) {
-    screen_1();
-  } else if (mode == 1) {
-    screen_2();
-  } else if (mode == 2) {
-    screen_3();
-  }
+  background(220);
+
+  // screen_1();
+  // screen_2();
+  screen_3();
+  // sendLeft.draw();
+  // sendRight.draw();
+  myButton.draw();
+
+
+
+  // if (screenMode == 0) {
+  //   screen_1();
+      // clear();
+  // } else if (screenMode == 1) {
+  //   screen_2();
+      // clear();
+  // } else if (screenMode == 2) {
+  //   screen_3();
+      // clear();
+  // }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
 
 function screen_1() {
-  //  choose product
-  text('Choose One Object')
-  text('Each object have different animations')
+  // https://editor.p5js.org/slow_izzm/sketches/VjpH74Oc1
+  // https://editor.p5js.org/slow_izzm/sketches/535SiO_aC
+  
+  // ellipse(width / 2, height / 2, 300, 300);
 
+  //  choose product
+  textSize(32);
+  text('Choose One Object', 10, 30);
+  textSize(20);
+  text('Each object have different animations', 10, 55);
+
+  
+
+  // products.artifacts.forEach(function (artifact) {
+  //   fill(artifact.color);
+  //   text(32)
+  //   text(artifact.name, 10, 75);
+  // });  
 }
 
 function screen_2() {
-  // write message box
-  text('Message Box')
-  text('Your message will not be saved')
+  noLoop();
+//   // rect(50, 50, 300, 300);
 
-  let messageField;
+//   // write message box
+  textSize(32);
+  text('Message Box', 10, 30);
+  textSize(20);
+  text('Your message will not be saved', 10, 55);
 
-  messageField = createInput('')
-  messageField.attribute('placeholder', 'Write your feelings / emotions / thoughts inside this message box to submit it with the object you selected')
-  messageField.position(100, 100)
-  messageField.size(100)
+  // let messageField;
+
+  // messageField = createInput('')
+  // messageField.attribute('placeholder', 'Write your feelings / emotions / thoughts inside this message box to submit it with the object you selected')
+  // messageField.position(100, 100)
+  // messageField.size(100)
   
-  let message = messageField.value()
+  // let message = messageField.value()
   
-  if (message){
-    textSize(24)
-  }
+  // if (message){
+  //   textSize(24)
+  // }
 }
 
 function screen_3() {
-  // send message
+
+  textSize(32);
+  text('Hold the screen on send your bottle', 10, 30);
+  textSize(16);
+  text('Your screen choice has no affect on your animations only on where it will show', 10, 55);
+
+  // create two equally sized div / button to be sent to Resolume display  
 }
 
 function primaryButton() {
   myButton = new Button({
     x: width/2, y: height/2,
-    width: width-30, height: 50,
-    align_x: 0, align_y: 0,
-    color: 'rgb(4834D4)',
-    content: 'Start Playing',
-    border_radius: '15',
-    // myButton.style('cursor', 'pointer'),
-
-    // on_press() {
-    //   window.open("https://www.google.com", '_parent')
-    // },
-    // on_click() {
-    //   window.open("https://www.google.com", '_parent')
-    // }
-    // on_mouse_enter() {
-    //   myButton.style('cursor', 'pointer')
-    // },
-    on_release() {
-      window.open("../pages/2_onboarding/onboarding.html", '_parent')
-    },
+    width: width, height: 50,
+    align_x: 0, aw: 0,
+    content: 'Clicks: 0',
+    on_press() {
+      count++;
+      myButton.text( 'Clicks: '+count );
+    }
   });
 }
+
+// //Method 2: Can only go in order
+// function nextButton() {
+//   myButton = new Button({
+//     x: width / 2, y: height / 2,
+//     width: width - 30, height: 50,
+//     align_x: 0, align_y: 0,
+//     color: 'rgb(000000)',
+//     content: 'Start Playing',
+//     border_radius: '15',
+//     // myButton.style('cursor', 'pointer'),
+
+//     on_press() {
+//       screenMode++;
+//       if (screenMode == 3) {
+//         screenMode = 0;
+//       }
+//       print(screenMode);
+//     },
+//   });
+// }
+
+// function previousButton() {
+//   myButton = new Button({
+//     x: width / 2, y: height / 2,
+//     width: width - 30, height: 50,
+//     align_x: 0, align_y: 0,
+//     color: 'rgb(000000)',
+//     content: 'Previous Page',
+//     border_radius: '15',
+//     // myButton.style('cursor', 'pointer'),
+
+//     on_press() {
+//       if (screenMode != 0) {
+//         screenMode--;
+//       }
+//       print(screenMode);
+//     },
+//   });
+// }
+
+// function leftScreen() {
+//   myButton = new Button({
+//     x: width/2, y: height/2,
+//     width: width, height: 50,
+//     align_x: 0, aw: 0,
+//     content: 'Clicks: 0',
+//     on_press() {
+//       count++;
+//       myButton.text( 'Clicks: '+count );
+//     }
+//   });
+// }
+
+// function rightScreen() {
+//   myButton = new Button({
+//     x: width/2, y: height/2,
+//     width: width, height: 50,
+//     align_x: 0, aw: 0,
+//     content: 'Clicks: 0',
+//     on_press() {
+//       count++;
+//       myButton.text( 'Clicks: '+count );
+//     }
+//   });
+// }
 
 
 ///////////////////////////////////////////////////////////////////
@@ -102,8 +211,8 @@ function initialiseResolume() {
 // tested within "draw". However, the steps included here should not be run 
 // every frame, to avoid too many OSC messages being sent to Resolume.
 //////////////////////////////////////////////////////////////////////////////
-function updateResolumeState() {
-}
+// function updateResolumeState() {
+// }
 
 
 /////////////////////////////////////////////////////////////////////
@@ -112,8 +221,8 @@ function updateResolumeState() {
 // This is like "draw", but applied to Resolume. In other words,
 // it runs over and over, every frame, after the website is loaded.
 /////////////////////////////////////////////////////////////////////
-function redrawResolumeComponents() {
-}
+// function redrawResolumeComponents() {
+// }
 
 ////////////////////////////////////////////////////
 // Helper functions:
@@ -128,17 +237,17 @@ function redrawResolumeComponents() {
 //						- layer: integer number of the layer we are setting the opacity of
 //						- opacityLevel: decimal number between 0.0 (full transparency) and 1.0 (full opacity)
 ////////////////////////////////////////////////////
-function loadClip(layer, clip) {
-	sendMessage("/composition/layers/" + layer + "/clips/" + clip + "/connect", 1, "f");
-}
+// function loadClip(layer, clip) {
+// 	sendMessage("/composition/layers/" + layer + "/clips/" + clip + "/connect", 1, "f");
+// }
 
-function turnLayerOff(layer) {
-	sendMessage("/composition/layers/" + layer + "/clear", 0, "f");
-}
+// function turnLayerOff(layer) {
+// 	sendMessage("/composition/layers/" + layer + "/clear", 0, "f");
+// }
 
-function setLayerOpacity(layer, opacityLevel) {
-	sendMessage("/composition/layers/" + layer + "/video/opacity", opacityLevel, "f");
-}
+// function setLayerOpacity(layer, opacityLevel) {
+// 	sendMessage("/composition/layers/" + layer + "/video/opacity", opacityLevel, "f");
+// }
 
 ////////////////////////////////////////////////////
 // CUSTOMIZABLE SECTION - END: ENTER OUR CODE HERE
