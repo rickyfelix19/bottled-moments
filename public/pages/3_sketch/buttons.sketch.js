afdcf7ff72d269160fed6b37ae0a70b305d599d4
Ricky Felix <rfel4230@uni.sydney.edu.au>
2023-05-11 14:46:48 +1000

    temporary seperate button

--- public/pages/3_sketch/buttons.sketch.js
+++ public/pages/3_sketch/buttons.sketch.js
@@ -1,64 +1,3 @@
-function primaryButton() {
-  myButton = new Button({
-    x: width / 2,
-    y: height / 2,
-    width: width - 30,
-    height: 50,
-    align_x: 0,
-    align_y: 0,
-    color: "rgb(4834D4)",
-    content: "Select Artifact",
-    border_radius: "15",
-    // myButton.style('cursor', 'pointer'),
-
-    // on_press() {
-    //   window.open("https://www.google.com", '_parent')
-    // },
-    // on_click() {
-    //   window.open("https://www.google.com", '_parent')
-    // }
-    // on_mouse_enter() {
-    //   myButton.style('cursor', 'pointer')
-    // },
-    on_release() {
-      // copy the product array and send it into user array
-      window.open("../pages/2_onboarding/onboarding.html", "_parent");
-    },
-  });
-}
-
-// selectButton
-function selectButton() {
-  function primaryButton() {
-    myButton = new Button({
-      x: width / 2,
-      y: height / 2,
-      width: width - 30,
-      height: 50,
-      align_x: 0,
-      align_y: 0,
-      color: "rgb(4834D4)",
-      content: "Select Artifact",
-      border_radius: "15",
-      // myButton.style('cursor', 'pointer'),
-
-      // on_press() {
-      //   window.open("https://www.google.com", '_parent')
-      // },
-      // on_click() {
-      //   window.open("https://www.google.com", '_parent')
-      // }
-      // on_mouse_enter() {
-      //   myButton.style('cursor', 'pointer')
-      // },
-      on_release() {
-        // copy the product array and send it into user array
-        window.open("../pages/2_onboarding/onboarding.html", "_parent");
-      },
-    });
-  }
-}
-
 // selectMessage
 function selectMessage() {
   myButton = new Button({
@@ -146,8 +85,8 @@ function backButton() {
     },
   });
 }
-// submitButton
-function submitButton() {
+// messageButton
+function messageButton() {
   myButton = new Button({
     x: width / 2,
     y: height / 2,
@@ -175,4 +114,13 @@ function submitButton() {
     },
   });
 }
-``;
+
+// leftButton
+function leftWall() {
+
+}
+
+// rightButton
+function rightWall() {
+
+}
--- public/pages/3_sketch/sketch.js
+++ public/pages/3_sketch/sketch.js
@@ -13,34 +13,34 @@ let artifacts = [
   {
     artifactID: 1,
     name: "shield",
-    category: "",
+    category: "concept",
     description: "",
     color: "red",
-    image: "www.google.com",
+    image: `www.google.com`,
   },
   {
     artifactID: 2,
     name: "rose",
-    category: "",
+    category: "plant",
     description: "",
     color: "red",
-    image: "www.google.com",
+    image: `www.google.com`,
   },
   {
     artifactID: 3,
     name: "carpet",
-    category: "",
+    category: "magical",
     description: "",
     color: "red",
-    image: "www.google.com",
+    image: `www.google.com`
   },
   {
     artifactID: 4,
     name: "flower",
-    category: "",
+    category: "plant",
     description: "",
     color: "red",
-    image: "www.google.com",
+    image: `www.google.com`,
   },
 ];
 
@@ -48,26 +48,36 @@ let artifacts = [
 //   let products = require("./products.js");
 // }
 
+function storeItem(artifact) {
+  selectedItems.push(artifact);
+}
+
 function setup() {
+  noLoop();
+
   p5 = createCanvas(windowWidth, windowHeight);
   p5.parent("container-p5");
 
-  let nextScreen1 = selectButton();
-  let nextScreen2 = selectMessage();
-  let skipScreen = skipButton();
-  let previousScreen = backButton();
-  let submit = submitButton();
+  // let nextScreen1 = selectButton();
+  // let nextScreen2 = selectMessage();
+  // let skipScreen = skipButton();
+  // let previousScreen = backButton();
+  // let submitMessage = submitButton();
 
   // create the following files:
   // selectButton
   // selectMessage
   // skipButton
   // backButton
-  // submitButton
+  // messageButton
+  // 
+
 }
 
 function draw() {
-  screen_1();
+  // screen_1();
+  // screen_2();
+  screen_3();
   // if (mode == 0) {
   //   mode = 1;
   // } else if (mode == 1) {
@@ -77,8 +87,14 @@ function draw() {
   // }
 }
 
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
 function screen_1() {
-  noLoop();
+  // skeleton loading
+
+
   //  choose product
   text("Choose One Object", 20, 30);
   text("Each object have different animations", 20, 50);
@@ -93,34 +109,85 @@ function screen_1() {
     // Display the item's name
     fill(0);
     text(artifact.name, itemLeft + 20, 100);
+        // Display the item's category
+    fill(128);
+    text(artifact.category, itemLeft + 20, 130);
+
+    // Display the item's description
+    fill(64);
+    text(artifact.description, itemLeft + 20, 160);
+
+    // Display the item's color
+    noStroke();
+    fill(artifact.color);
+    rect(itemLeft + 20, 180, 30, 30);
+
+    // Display the item's image
+    let img = loadImage(artifact.image);
+    image(img, itemLeft + 80, 180, 50, 50);
   });
+  
+  // Add a button for each item
+  artifacts.forEach((artifact, index) => {
+    let button = createButton(artifact.name);
+    button.position(20 + index * itemWidth, 20);
+    button.mousePressed(() => {
+      storeItem(item);
+    });
+  });
+
+  // Display the selected items
+  fill(0);
+  text('Selected items:', 20, 300);
+  selectedItems.forEach((artifact, index) => {
+    text(artifact.name, 20, 330 + index * 30);
+  });
+}
+
+
   // map product.json data into here and then add styling into it
 
   // nextScreen1.draw();
-}
 
 function screen_2() {
-  // write message box
-  text("Message Box");
-  text("Your message will not be saved");
+  // skeleton loading
 
+  // message checkers
+  var regex = /^(?i)(?:\b\w+\b\s*){1,20}$/;
   let messageField;
+
+  let message = messageField.value();
+  var match = input.match(message);
+
+  // write message box
+  text("Message Box", 20, 30);
+  text("Your message will not be saved", 20, 50);
 
   messageField = createInput("");
   messageField.attribute(
     "placeholder",
     "Write your feelings / emotions / thoughts inside this message box to submit it with the object you selected"
   );
-  messageField.position(100, 100);
-  messageField.size(100);
+  messageField.position(windowWidth/2, windowHeight/2);
+  messageField.size(windowWidth/3, windowHeight/3);
 
-  let message = messageField.value();
-
-  if (message) {
+  if (match) {
     textSize(24);
   }
 }
 
 function screen_3() {
   // send message
+  text("Send your Artifact", 20, 30);
+  text("By holding the screens", 20, 50);
+
+  // left side
+  // right side
+
+  // loading screen
+}
+
+// storeItem(key, value)
+function storeItem(artifact) {
+  selectedItems.push(artifact);
 }
